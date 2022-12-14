ba867d44e5b04429eb8eb928e91dcf1762c06b15
virat2110 <83300897+virat2110@users.noreply.github.com>
2022-12-15 03:55:34 +0530

    Update settings.py

--- sampleApp/settings.py
+++ sampleApp/settings.py
@@ -187,8 +187,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "viratanand2001@gmail.com"
-EMAIL_HOST_PASSWORD = "9473343462v"
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 
 
 # Time out:-
